94983c9bdf953d888273279f2774a34d4c2e3b0e
Mike Meredith <mmeredith@wcsmsia>
2019-05-06 19:12:32 +0800

    Tweaks to plotACs

--- R/plotACs.R
+++ R/plotACs.R
@@ -21,32 +21,47 @@ plotACs <- function(
   y <- colMeans(ACs[, ,2], na.rm=TRUE)
 
   # Recover animal IDs if possible
-  ncap <- nrow(Y) # number of animals captured
   M <- dim(ACs)[2] # total, incl. uncaptures
   animalIDs <- sprintf("id%03d", 1:M)
-  if(!is.null(rownames(Y)))
-    animalIDs[1:nrow(Y)] <- rownames(Y)
+  captLocList <- NULL
+  if(!missing(Y)) {
+    ncap <- nrow(Y) # number of animals captured
+    if(!is.null(rownames(Y)))
+      animalIDs[1:nrow(Y)] <- rownames(Y)
+    # which to plot
+    if(any(is.na(which)))
+      which <- 1:ncap
 
-  # which to plot
-  if(any(is.na(which)))
-    which <- 1:ncap
-
-  # Get capture locations
-  captLocList <- vector('list', ncap)
-  for(i in 1:ncap) {
-    captTraps <- which(Y[i, ] > 0) # Which traps caught the animal
-    captLocList[[i]] <- traps[captTraps, , drop=FALSE] # Locations of the traps
+    # Get capture locations
+    if(!missing(traps)) {
+      captLocList <- vector('list', ncap)
+      for(i in 1:ncap) {
+        captTraps <- which(Y[i, ] > 0) # Which traps caught the animal
+        captLocList[[i]] <- traps[captTraps, , drop=FALSE] # Locations of the traps
+      }
+    }
   }
-
+  if(any(is.na(which)))
+    which <- 1:M
   # do the plot
-  plot(hab)
-  points(traps, pch=3, col='red')
+  MASS::eqscplot(x, y, type='n', xlim=range(ACs[, , 1], na.rm=TRUE),
+    ylim=range(ACs[, , 2], na.rm=TRUE),
+    ann=FALSE, axes=FALSE)
+  if(!missing(hab)) {
+    if(inherits(hab, "SpatialPolygons")) {
+    sp::plot(hab, add=TRUE)
+    } else if((is.matrix(hab) || is.data.frame(hab)) && ncol(hab) == 2) {
+      lines(hab)
+    }
+  }
+  if(!missing(traps))
+    points(traps, pch=3, col='red')
   colors <- palette()[-1]
   colno <- 1
   for(i in which) {
     col <- colors[colno]
     points(ACs[, i, ], cex=0.1, col=adjustcolor(col, 0.3))
-    if(i <= ncap)
+    if(i <= ncap && !is.null(captLocList))
       points(captLocList[[i]], pch=21, col='black', bg=col, cex=1.2)
     colno <- colno+1
     if(colno > length(colors))
