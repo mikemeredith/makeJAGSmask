10ac7f6e8074dc327bb3f93bb1975e327445f8e8
mikemeredith <mmeredith@wcs.org>
2020-07-23 23:06:30 +0800

    Update @making_makeJAGSmask_package.R

--- .local_checks/@making_makeJAGSmask_package.R
+++ .local_checks/@making_makeJAGSmask_package.R
@@ -1,30 +1,30 @@
 
-setwd("D:/Github/AHMbook_package") # my desktop
-setwd("C:/Github/AHMbook_package") # my laptop
+setwd("D:/Github/makeJAGSmask_package") # my desktop
+setwd("C:/Github/makeJAGSmask_package") # my laptop
 dir()
 
 # Spelling check
 library(spelling)
-update_wordlist(pkg = "AHMbook", confirm = TRUE)
-out <- spell_check_package(pkg = "AHMbook")
+update_wordlist(pkg = "makeJAGSmask", confirm = TRUE)
+out <- spell_check_package(pkg = "makeJAGSmask")
 
 # Misc checks
 library(tools)
-checkTnF("AHMbook")
+checkTnF("makeJAGSmask")
 
 # Install dependencies
-install.packages(c("plotrix", "raster", "RandomFields", "coda",
-    "unmarked", "mvtnorm", "spdep"))
+install.packages(c("abind", "plotrix", "raster",
+    "secr", "sp", "rgeos"))
 
 # For package development
-system("R CMD INSTALL AHMbook") # Use this for a "dev" install.
-devtools::load_all("AHMbook")
+system("R CMD INSTALL makeJAGSmask") # Use this for a "dev" install.
+devtools::load_all("makeJAGSmask")
 
-# Create the AHMbook package
+# Create the makeJAGSmask package
 # ==========================
 unlink(list.files(pattern="Rplots.pdf", recursive=TRUE))
-system("R CMD build AHMbook")  # Produces the .tar.gz
-pkg <- "AHMbook_0.2.0.9004.tar.gz"  # <-- fix version number here
+system("R CMD build makeJAGSmask")  # Produces the .tar.gz
+pkg <- "makeJAGSmask_0.1.1.9001.tar.gz"  # <-- fix version number here
 
 # Pick one to check:
 ## on desktop
@@ -38,38 +38,9 @@ system(paste("R CMD check ", pkg, "--as-cran --no-manual"))
 system(paste("R CMD INSTALL ", pkg))            # install only
 system(paste("R CMD INSTALL ", pkg, "--build")) # install and produce the .zip binary
 
+library(testthat)
+test_package("makeJAGSmask", reporter=ProgressReporter)
 
 # Try it out:
-library(AHMbook)
-?AHMbook
-
-
-# Check graphics:
-tmp <- simNmix()
-data <-  simNmix(nsites = 130, nvisits = 2, mean.lam = 120, mean.p = 0.7,
-            sigma.lam = 0.6, sigma.p.site = 0.26,  sigma.p.survey = 0.37,
-	          show.plot = TRUE)
-
-
-
-example(simDSM)
-example(simExpCorrRF)
-example(simOccSpatial)
-example(BerneseOberland)
-
-data(MesoCarnivores)
-str(MesoCarnivores)
-# display the species names:
-dimnames(SwissAtlasHa$counts)[[3]]
-
-str(simPH())
-
-library(AHMbook)
-data(wigglyLine)
-points <- sp::SpatialPoints( wigglyLine )
-sLine <- sp::Line(points)
-regpoints <- sp::spsample(sLine, 100, type = "regular")
-set.seed(2027, kind = "Mersenne-Twister") # Fig 11.15 in the book
-tmp <- simDSM(X = regpoints@coords)
-str(tmp)
-
+library(makeJAGSmask)
+?makeJAGSmask
